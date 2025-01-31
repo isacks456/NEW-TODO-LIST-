1b44ecca2521a1648769b882eb97677e8cf94ba0
Исмаил Исмаилов <ismailismailov@MacBook-Air-Ismail.local>
2025-02-01 00:23:49 +0300

    Added elements to the Home page

--- src/components/pages/AboutUs/AboutUs.jsx
+++ src/components/pages/AboutUs/AboutUs.jsx
@@ -1,5 +1,17 @@
+import styles from './About.module.scss'
+
 export const AboutUs = () => {
 	return (
-		<div>AboutUs</div>
+		<div className={styles.AboutUs}>
+			<div className={styles.subAboutUs}>
+				On this site you can create tasks.
+			</div>
+			<div className={styles.subAboutUs1}>
+				<div className={styles.subAboutUs2}>
+					This is my GitHub
+					<a href="https://github.com/isacks456" className={styles.linkAboutUs}><img src="/public/github-logo.png" alt="GitHub" className={styles.imageAboutUs} />GitHub</a>
+				</div>
+			</div>
+		</div>
 	)
 }
