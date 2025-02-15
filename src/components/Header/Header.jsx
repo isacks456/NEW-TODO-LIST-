423d3de2eadb326fb615c5441f252e0fa26a3b2c
Исмаил Исмаилов <ismailismailov@MacBook-Air-Ismail.local>
2025-02-15 16:19:03 +0300

    Added menu for mobile devices

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,29 @@
-import { NavLink } from "react-router-dom"
-import styles from './Header.module.scss'
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './Header.module.scss';
 
 export const Header = () => {
-	return (
-		<div className={styles.Header}>
-			<div className={styles.subHeader}>ToDo-List</div>
-			<span className={styles.spanHeader}>
-				<NavLink to='/' className={styles.linkHeader}>Home</NavLink>
-				<NavLink to='/aboutus' className={styles.linkHeader}>About Us</NavLink>
-				<NavLink to='/todolist' className={styles.linkHeader}>ToDo-List</NavLink>
-			</span>
-		</div>
-	)
-}
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className={styles.Header}>
+      <div className={styles.subHeader}>ToDo-List</div>
+      <div className={`${styles.burgerIcon} ${isMenuOpen ? styles.open : ''}`} onClick={toggleMenu}>
+  			<span className={`${styles.spanHeader2} ${isMenuOpen ? styles.open1 : ''}`}></span>
+				<span className={`${styles.spanHeader2} ${isMenuOpen ? styles.open1 : ''}`}></span>
+				<span className={`${styles.spanHeader2} ${isMenuOpen ? styles.open1 : ''}`}></span>
+			</div>
+      <span className={`${styles.spanHeader} ${isMenuOpen ? styles.open : ''}`}>
+        <div className={styles.spanHeader1}>
+          <NavLink to='/' className={styles.linkHeader} onClick={toggleMenu}>Home</NavLink>
+          <NavLink to='/aboutus' className={styles.linkHeader} onClick={toggleMenu}>About Us</NavLink>
+          <NavLink to='/todolist' className={styles.linkHeader} onClick={toggleMenu}>ToDo-List</NavLink>
+        </div>
+      </span>
+    </div>
+  );
+};
